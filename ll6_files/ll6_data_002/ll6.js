24f37c4c75b71537bde464f8f97fb349cf28d673
ghoul-cpu <ahmednsry5@gmail.com>
2025-11-13 06:40:17 +0200

    Delete ll6_files directory

--- ll6_files/ll6_data_002/ll6.js
+++
@@ -1,4 +0,0 @@
-var ChatServer = '147.135.251.108';
-var ChatSiteID = 1004;
-var ChatPorts = '6039,6040,6041,6042';
-document.write('<scr' + 'ipt language="javascript" type="text/javascript" src="http://' + ChatServer + '/DigiChat/__.js?ts=' + (new Date).getTime() + '"></sc' + 'ript>');
